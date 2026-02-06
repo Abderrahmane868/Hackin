4f4669a9166989c996eab9a8018405969bc3a7bd
yasserlakrid <yasserlakrid36@gmail.com>
2026-02-06 09:49:06 +0100

    main

---
+++ src/app/Main/page.jsx
@@ -0,0 +1,13 @@
+import Image  from "next/image";
+function Main(){
+    return(
+
+        <>
+        <div className="MainPage">
+            
+        </div> 
+        </>
+    );
+
+}
+export default Main;
--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Signup from "../components/Signup"; 
 import Signin from "../components/Signin";
 import Verification from "../components/Verification";
+import Main from "./Main/page";
 export default function Home() {
   return (
    <>
-  <Verification/> 
+  <Main/> 
    </>
   );
 }
