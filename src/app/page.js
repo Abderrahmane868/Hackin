f5086431ee6eb0c57f8e78ae7f69b14f934c88f8
yasserlakrid <yasserlakrid36@gmail.com>
2026-02-06 09:38:23 +0100

    add verification page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Signup from "../components/Signup"; 
 import Signin from "../components/Signin";
+import Verification from "../components/Verification";
 export default function Home() {
   return (
    <>
-  <Signin/> 
+  <Verification/> 
    </>
   );
 }
